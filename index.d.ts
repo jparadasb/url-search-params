868e48e23b86dfd1562b8b463c2ec0a314beab32
Jose Paradas <jparadas.b@gmail.com>
2017-03-27 14:43:59 -0300

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+declare class URLSearchParams {
+    constructor(init?: string| URLSearchParams);
+
+    append(name: string, value: string): void
+
+    delete(name: string): void;
+
+    entries(): IterableIterator<[string, string]>;
+
+    get(name: string): string;
+
+    getAll(name: string): string[];
+
+    has(name: string): boolean;
+
+    keys(): IterableIterator<string>;
+
+    set(name: string, value: string): void;
+
+    toString(): string;
+
+    values(): IterableIterator<string>;
+
+    [Symbol.iterator](): IterableIterator<number>;
+}
